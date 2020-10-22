df6e1efa22ac01ba2fa617eaa39eb19cf7095657
Sidnei Alves <sidneialves2004@yahoo.com.br>
2020-10-22 14:59:05 -0300

    first commit

--- dist/controllers/OrphanagesController.js
+++ dist/controllers/OrphanagesController.js
@@ -23,27 +23,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const typeorm_1 = require("typeorm");
-const Orphanage_1 = __importDefault(require("./models/Orphanage"));
-const orphanages_view_1 = __importDefault(require("./views/orphanages_view"));
+const Orphanage_1 = __importDefault(require("../models/Orphanage"));
+const orphanages_view_1 = __importDefault(require("../views/orphanages_view"));
 const Yup = __importStar(require("yup"));
 // query params http://localhost:3333/user?search=sidnei
 // route params http://localhost:3333/user/1   //identificar dado dentro de um recurso (:id - dois pontos usaado para indicar row params)
 // body         http://localhost:3333/user   //enviar dados que nao cabem na url
 exports.default = {
     async index(request, response) {
-        const orphanagesRepository = typeorm_1.getRepository(Orphanage_1);
+        const orphanagesRepository = typeorm_1.getRepository(Orphanage_1.default);
         const orphanages = await orphanagesRepository.find({
             relations: ['images']
         });
-        return response.json(orphanages_view_1.renderMany(orphanages));
+        return response.json(orphanages_view_1.default.renderMany(orphanages));
     },
     async show(request, response) {
         const { id } = request.params;
-        const orphanagesRepository = typeorm_1.getRepository(Orphanage_1);
+        const orphanagesRepository = typeorm_1.getRepository(Orphanage_1.default);
         const orphanage = await orphanagesRepository.findOneOrFail(id, {
             relations: ['images']
         });
-        return response.json(orphanages_view_1.render(orphanage));
+        return response.json(orphanages_view_1.default.render(orphanage));
     },
     async create(request, response) {
         const { name, latitude, longitude, about, instructions, opening_hours, open_on_weekends } = request.body;
@@ -76,7 +76,7 @@ exports.default = {
         await schema.validate(data, {
             abortEarly: false,
         });
-        const orphanagesRepository = typeorm_1.getRepository(Orphanage_1);
+        const orphanagesRepository = typeorm_1.getRepository(Orphanage_1.default);
         const orphanage = orphanagesRepository.create(data);
         await orphanagesRepository.save(orphanage);
         return response.status(201).json(orphanage);
