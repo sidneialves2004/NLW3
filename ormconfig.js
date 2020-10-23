c8fa02e274381287e995a24eeb8b3eb87b982f06
Sidnei Alves <sidneialves2004@yahoo.com.br>
2020-10-23 08:22:06 -0300

    first commit

--- ormconfig.js
+++ ormconfig.js
@@ -6,12 +6,12 @@ console.log(`${path.default.join(__dirname,'dist','database', )}/database.sqlite
 module.exports =
 {
     "type": "sqlite",
-    "database":`${path.default.join(__dirname,'dist','database', )}/database.sqlite` ,
+    "database":`${path.default.join(__dirname,'dist','database')}/database.sqlite` ,
     "migrations": [
       "dist/database/migrations/*.js"
     ],
     "entities": [
-      "dist/models/*.js"
+      `${path.default.join(__dirname,'dist','models' )}/*.js`
     ],
     "cli": {
       "migrationsDir": "./dist/database/migrations"
